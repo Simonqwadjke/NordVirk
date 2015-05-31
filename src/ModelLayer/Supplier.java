d609f19e5fe90c02bd83b04e69e512cb0ec8836e
Simonqwadjke <Simonqwadjke@gmail.com>
2015-05-31 21:53:49 +0200

    editet supplier

--- src/ModelLayer/Supplier.java
+++ src/ModelLayer/Supplier.java
@@ -2,4 +2,13 @@ package ModelLayer;
 
 public class Supplier {
 
+	private Contact contact;
+	
+	public Contact getContact(){
+		return this.contact;
+	}
+	
+	public void setContact(Contact contact){
+		this.contact = contact;
+	}
 }
