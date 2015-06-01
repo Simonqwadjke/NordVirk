eba1c9b8655e2a03d07445410886911ed01eb7b1
Simonqwadjke <Simonqwadjke@gmail.com>
2015-06-02 00:15:38 +0200

    Changed person to interface

--- src/DBLayer/DBPerson.java
+++ src/DBLayer/DBPerson.java
@@ -5,59 +5,25 @@ import java.util.ArrayList;
 
 import ModelLayer.*;
 
-public class DBPerson {
+public interface DBPerson {
+	
+	public ArrayList<Person> findPersonByName (String name);
+	
+	public ArrayList<Person> findPersonByPhoneNumber (String phoneNumber);
+	
+	public ArrayList<Person> findPersonByEmail (String email);
+	
+	public ArrayList<Person> findPersonByAddress (String address);
+	
+	public void insertPerson (String Person);
+	
+	public void updatePerson (String Person);
+	
+	public void deletePerson (int ID);
+	
+	public ArrayList<Person> search (String wClause);
+	
+	public Supplier buildPerson() throws SQLException;
 
-	private Connection con;
-	
-	public ArrayList<> findPersonByName (String name)
-	{
-		
-	}
-	
-	public ArrayList<> findPersonByPhoneNumber (String phoneNumber)
-	{
-		
-	}
-	
-	public ArrayList<> findPersonByEmail (String email)
-	{
-		
-	}
-	
-	public ArrayList<> findPersonByAddress (String address)
-	{
-		
-	}
-	
-	public void insertPerson (String Person)
-	{
-		
-	}
-	
-	public void updatePerson (String Person)
-	{
-		
-	}
-	
-	public void deletePerson (int ID)
-	{
-		
-	}
-	
-	public ArrayList<> search (String wClause)
-	{
-		
-	}
-	
-	public Supplier buildPerson() throws SQLException{
-		
-	}
-
-	public String buildQuery(String wClause){
-		String query = "";
-		if(wClause.length() > 0){
-			query = query + " WHERE " + wClause;
-		}
-		return query;
-	}
+	public String buildQuery(String wClause);
 }
