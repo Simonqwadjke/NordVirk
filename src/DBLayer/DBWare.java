c01f975295066f45ed833c8bade6de265b2e4c19
Grilaznar <martin.soe.kristensen@mvb.net>
2015-06-01 23:49:53 +0200

    DBWare fix

--- src/DBLayer/DBWare.java
+++ src/DBLayer/DBWare.java
@@ -32,6 +32,47 @@ public class DBWare{
 		return search(wClause);
 	}
 	
+	public void insert(Ware ware, int listID) throws SQLException{
+		
+		String query = "INSERT INTO Ware(listID,name,status,signature,serialNUmber) "
+				+ "VALUES('" 
+				+ listID + "','"
+				+ ware.getName() + "','"
+				+ ware.getStatus() + "','"
+				+ ware.getSignature() + "','"
+				+ ware.getSerialNumber() + "')";
+		
+		Statement stmt = con.createStatement();
+		stmt.setQueryTimeout(5);
+		stmt.executeQuery(query);
+		stmt.close();
+	}
+	
+	public void update(Ware ware)throws SQLException{
+		
+		String query = "UPDATE Ware SET "
+				+ "name = '" + ware.getName() + "', "
+				+ "status = '" + ware.getStatus() + "', "
+				+ "signature = '" + ware.getSignature() + "', "
+				+ "serialNumber = '" + ware.getSerialNumber() + "' "
+				+ "WHERE wareID = " + ware.getID();
+		
+		Statement stmt = con.createStatement();
+		stmt.setQueryTimeout(5);
+		stmt.executeQuery(query);
+		stmt.close();
+	}
+	
+	public void delete(Ware ware)throws SQLException{
+
+		String query = "DELETE FROM Ware WHERE wareID = " + ware.getID();
+		
+		Statement stmt = con.createStatement();
+		stmt.setQueryTimeout(5);
+		stmt.executeQuery(query);
+		stmt.close();
+	}
+	
 	private ArrayList<Ware> search(String wClause) throws SQLException{
 		ResultSet results;
 		ArrayList<Ware> wares = new ArrayList<Ware>();
@@ -54,7 +95,7 @@ public class DBWare{
 	private Ware buildWare(ResultSet results) throws SQLException{
 		Ware ware = new Ware();
 		
-		ware.setID(results.getInt("ID"));
+		ware.setID(results.getInt("wareID"));
 		ware.setName(results.getString("name"));
 		ware.setSerialNumber(results.getString("serialNumber"));
 		ware.setSignature(results.getString("signature"));
