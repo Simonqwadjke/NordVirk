3fb2442394966eacfd7b00e46cea8d949928df7b
ZohdiAF <ghandour_zohdi@hotmail.com>
2015-06-01 10:36:16 +0200

    Finished Contact class

--- src/ModelLayer/Contact.java
+++ src/ModelLayer/Contact.java
@@ -1,5 +1,27 @@
 package ModelLayer;
 
 public class Contact extends Person{
+	
+	private supplier Supplier;
+	
+	public Contact()
+	{
+		supplier = null;
+	}
+	
+	public Contact(Supplier supplier)
+	{
+		this.supplier =  supplier;
+	}
+	
+	public supplier getSupplier()
+	{
+		return supplier;
+	}
+	
+	public void setSupplier(supplier Supplier)
+	{
+		this.supplier = supplier;
+	}
 
 }
