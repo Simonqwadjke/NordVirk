6ccc28a0075a525331f851e7f072a0e3e85480dc
andersboergesen <andersboergesen@gmail.com>
2015-06-01 10:51:24 +0200

    Finish Employee
    Finished

--- src/ModelLayer/Employee.java
+++ src/ModelLayer/Employee.java
@@ -1,5 +1,25 @@
 package ModelLayer;
 
 public class Employee extends Person{
-
+	private int rights;
+	
+	public Employee()
+	{
+		rights = 0;
+	}
+	
+	public Employee(int rights)
+	{
+	this.rights = rights;
+	}
+	
+	public int getRights()
+	{
+		return rights;
+		}
+	
+	public void setRights (int rights)
+	{
+		this.rights = rights;
+		}
 }
