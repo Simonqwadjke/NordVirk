6496989ee3a3d058b437870425503cf96eafe583
Grilaznar <martin.soe.kristensen@mvb.net>
2015-06-05 00:47:29 +0200

    add some throws

--- src/DBLayer/DBPerson.java
+++ src/DBLayer/DBPerson.java
@@ -7,21 +7,21 @@ import ModelLayer.*;
 
 public interface DBPerson {
 	
-	public ArrayList<Person> findByName (String name);
+	public ArrayList<Person> findByName (String name) throws SQLException;
 	
-	public ArrayList<Person> findByPhoneNumber (String phoneNumber);
+	public ArrayList<Person> findByPhoneNumber (String phoneNumber) throws SQLException;
 	
-	public ArrayList<Person> findByEmail (String email);
+	public ArrayList<Person> findByEmail (String email) throws SQLException;
 	
-	public ArrayList<Person> findByAddress (String address);
+	public ArrayList<Person> findByAddress (String address) throws SQLException;
 	
-	public void insert(String Person);
+	public void insert(Person person) throws SQLException;
 	
-	public void update(String Person);
+	public void update(Person person) throws SQLException;
 	
-	public void delete(int ID);
+	public void delete(Person person) throws SQLException;
 	
-	public ArrayList<Person> search (String wClause);
+	public ArrayList<Person> search (String wClause) throws SQLException;
 	
 	public Supplier buildPerson() throws SQLException;
 
