7a5803c1eb47d709451dbdcd1a0f5f23f8a5766f
Grilaznar <martin.soe.kristensen@mvb.net>
2015-06-07 17:33:19 +0200

    tror den er færdig

--- src/DBLayer/DBEmployee.java
+++ src/DBLayer/DBEmployee.java
@@ -14,32 +14,32 @@ public class DBEmployee{
 	}
 
 	public ArrayList<Employee> findByName(String name)  throws SQLException{
-		String wClause = "name = " + name;
+		String wClause = " name = " + name;
 		return search(wClause);
 	}
 
 	public ArrayList<Employee> findByPhoneNumber(String phoneNumber)  throws SQLException{
-		String wClause = "phoneNumber = " + phoneNumber;
+		String wClause = " phoneNumber = " + phoneNumber;
 		return search(wClause);
 	}
 
 	public ArrayList<Employee> findByEmail(String email)  throws SQLException{
-		String wClause = "email = " + email;
+		String wClause = " email = " + email;
 		return search(wClause);
 	}
 
 	public ArrayList<Employee> findByAddress(String address)  throws SQLException{
-		String wClause = "address = " + address;
+		String wClause = " address = " + address;
 		return search(wClause);
 	}
 	
 	public ArrayList<Employee> findByRights(int rights)  throws SQLException{
-		String wClause = "rights = " + rights;
+		String wClause = " rights = " + rights;
 		return search(wClause);
 	}
 
-	public void insert(Employee employee) throws SQLException {
-		String query = "INSERT INTo Person(name,phoneNumber,email,address,subtype)"
+	public void insert(Employee employee) throws SQLException{
+		String query = " INSERT INTo Person(name,phoneNumber,email,address,subtype)"
 				+ " VALUES('" 
 				+ employee.getName() + "','"
 				+ employee.getPhoneNr() + "','"
@@ -50,32 +50,84 @@ public class DBEmployee{
 		Statement stmt = con.createStatement();
 		stmt.setQueryTimeout(5);
 		stmt.executeQuery(query);
+		query = " INSERT INTO Employee(personID,rights)"
+				+ " VALUES('"
+				+ stmt.executeQuery("SELECT personID FROM Person"
+						+ " WHERE "
+						+ " name = " + employee.getName()
+						+ " phoneNumber = " + employee.getPhoneNr()
+						+ " subtype = Employee").getInt(1) 
+				+ "','"	+ employee.getRights() + "')";
+		stmt.executeQuery(query);
 		stmt.close();
 	}
 
-	public void update(Employee employee) throws SQLException {
-		// TODO Auto-generated method stub
-
+	public void update(Employee employee) throws SQLException{
+		String query = " UPDATE Person SET"
+				+ " name = " + employee.getName()
+				+ " phoneNumber = " + employee.getPhoneNr()
+				+ " email = " + employee.getEmail()
+				+ " address = " + employee.getAddress()
+				+ " subtype = employee"
+				+ " WHERE personID = " + employee.getID()
+				+ " UPDATE Employee SET"
+				+ " rights = " + employee.getRights()
+				+ " WHERE personID = " + employee.getID();
+		
+		Statement stmt = con.createStatement();
+		stmt.setQueryTimeout(5);
+		stmt.executeQuery(query);
+		stmt.close();
 	}
 
-	public void delete(Employee employee) {
-		// TODO Auto-generated method stub
-
+	public void delete(Employee employee) throws SQLException{
+		String query = " DELETE FROM Employee WHERE personID = " + employee.getID()
+				+ " DELETE FROM Person WHERE personID = " + employee.getID();
+		
+		Statement stmt = con.createStatement();
+		stmt.setQueryTimeout(5);
+		stmt.executeQuery(query);
+		stmt.close();
 	}
 
-	public ArrayList<Employee> search(String wClause) {
-		// TODO Auto-generated method stub
-		return null;
+	public ArrayList<Employee> search(String wClause) throws SQLException{
+		ResultSet results;
+		ArrayList<Employee> emps = new ArrayList<Employee>();
+		
+		String query = buildQuery(wClause);
+		
+		Statement stmt = con.createStatement();
+		stmt.setQueryTimeout(5);
+		results = stmt.executeQuery(query);
+		while(results.next()){
+			Employee emp;
+			emp = buildEmployee(results);
+			emps.add(emp);
+		}
+		stmt.close();
+		
+		return emps;
 	}
 
-	public Supplier buildEmployee() throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+	public Employee buildEmployee(ResultSet results) throws SQLException{
+		Employee emp = new Employee();
+		
+		emp.setID(results.getInt("personID"));
+		emp.setName(results.getString("name"));
+		emp.setPhoneNr(results.getString("phoneNumber"));
+		emp.setAdress(results.getString("address"));
+		emp.setEmail(results.getString("email"));
+		emp.setRights(results.getInt("rights"));
+		
+		return emp;
 	}
 
 	public String buildQuery(String wClause) {
-		// TODO Auto-generated method stub
-		return null;
+		String query = " SELECT personID,name,phoneNumber,address,email,rights FROM Person,Employee";
+		if(wClause.length() > 0){
+			query = query + " WHERE" + wClause;
+		}
+		return query;
 	}
 
 }
