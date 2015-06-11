8d9ae176b04eb18782a29495e8d41e9da9d38c3d
Grilaznar <martin.soe.kristensen@mvb.net>
2015-06-11 12:48:59 +0200

    delete var træls

--- src/DBLayer/DBContact.java
+++ src/DBLayer/DBContact.java
@@ -38,7 +38,7 @@ public class DBContact {
 		return search(wClause);
 	}
 
-	public void insert(Contact contact, Supplier supplier) throws SQLException {
+	public void insert(Contact contact) throws SQLException {
 		String query = " INSERT INTO Person(name,phoneNumber,email,address,subtype)"
 				+ " VALUES('" 
 				+ contact.getName() + "','"
@@ -54,26 +54,58 @@ public class DBContact {
 		
 		Statement stmt = con.createStatement();
 		stmt.setQueryTimeout(5);
+		System.out.println(query);
 		stmt.executeUpdate(query);
 		ResultSet idRow = stmt.executeQuery(getID);
 		idRow.next();
 		stmt.executeUpdate(" INSERT INTO Contact(personID) VALUEs('"
 				+ idRow.getInt(1) + "'");
-		if(supplier != null){
+		if(contact.getSupplier() != null){
 			idRow = stmt.executeQuery("SELECT contactID FROM Contacts WHERE personID = '" + idRow.getInt(1) + "'");
 			idRow.next();
 			stmt.executeUpdate(" INSERT INTO SupplierContacts(contactID,supplierID) VALUES('"
-					+ idRow.getInt(1) + "','" + supplier.getID() + "')");
+					+ idRow.getInt(1) + "','" + contact.getSupplier().getID() + "')");
 		}
 		stmt.close();
 	}
 
 	public void update(Contact contact) throws SQLException {
+		String query = " UPDATE Person SET"
+				+ " name = '" + contact.getName() + "',"
+				+ " phoneNumber = '" + contact.getPhoneNr() + "'"
+				+ " email = '" + contact.getEmail() + "'"
+				+ " address = '" + contact.getAddress() + "'"
+				+ " subtype = 'Contact'"
+				+ " WHERE personID = " + contact.getID()
+				+ " UPDATE SupplierContacts SET"
+				+ " supplierID = " + contact.getSupplier().getID()
+				+ " WHERE contactID = " + contact.getID();
 		
+		Statement stmt = con.createStatement();
+		stmt.setQueryTimeout(5);
+		System.out.println(query);
+		stmt.executeUpdate(query);
+		stmt.close();
 	}
 
 	public void delete(Contact contact) throws SQLException {
-		// TODO Auto-generated method stub
+		String query = " DELETE FROM Person WHERE"
+				+ " personID = " + contact.getID()
+				+ " DELETE FROM Contact WHERE"
+				+ " personID = " + contact.getID();
+		String getID = " SELECT contactID FROM Contact WHERE personID = " + contact.getID();
+
+		Statement stmt = con.createStatement();
+		stmt.setQueryTimeout(5);
+		System.out.println(getID);
+		ResultSet idRow = stmt.executeQuery(getID);
+		idRow.next();
+		System.out.println(query);
+		stmt.executeUpdate(query);
+		query = " DELETE FROM SupplierContacts WHERE contactID = " + idRow.getInt(1);
+		System.out.println(query);
+		stmt.executeUpdate(query);
+		stmt.close();
 
 	}
 
